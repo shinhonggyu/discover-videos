a83deeeadd3f311cc66ac73838ad8f8576b12bd9
shinhonggyu <zowoz8819@gmail.com>
2022-05-18 20:50:21 +0900

    navbar dropdown

---
+++ components/navbar/navbar.js
@@ -0,0 +1,59 @@
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Navbar = (props) => {
+  const { username } = props;
+
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>Netflix</div>
+          </a>
+        </Link>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+              {/* Expand more icon */}
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <Link href="/login">
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
