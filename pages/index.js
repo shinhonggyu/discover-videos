28ab1ec15b525e6a301729fdcac084a663dbf983
shinhonggyu <zowoz8819@gmail.com>
2022-05-22 01:58:53 +0900

    Implement Popular Videos

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,32 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import Navbar from "../components/navbar/navbar";
 import SectionCards from "../components/card/section-cards";
-import { getVideos } from "../lib/videos";
+import { getVideos, getPopularVideos } from "../lib/videos";
 
 export async function getServerSideProps(context) {
   const disneyVideos = await getVideos("disney trailer");
   const productivityVideos = await getVideos("productivity");
   const travelVideos = await getVideos("travel");
-  // const popularVideos = await getVideos("disney trailer");
+  const USpopularVideos = await getPopularVideos("US");
+  const KRpopularVideos = await getPopularVideos("KR");
   return {
     props: {
       disneyVideos,
       productivityVideos,
       travelVideos,
+      USpopularVideos,
+      KRpopularVideos,
     },
   };
 }
 
-const Home = ({ disneyVideos, productivityVideos, travelVideos }) => {
+const Home = ({
+  disneyVideos,
+  productivityVideos,
+  travelVideos,
+  USpopularVideos,
+  KRpopularVideos,
+}) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -28,22 +37,25 @@ const Home = ({ disneyVideos, productivityVideos, travelVideos }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar username="honggyu@gmail.com" />
-      <Banner
-        title="Clifford the Big Red Dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford.webp"
-      />
-
-      <div className={styles.sectionWrapper}>
-        <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Travel" videos={travelVideos} size="small" />
-        <SectionCards
-          title="Productivity"
-          videos={productivityVideos}
-          size="medium"
+      <div className={styles.main}>
+        <Navbar username="honggyu@gmail.com" />
+        <Banner
+          title="Clifford the Big Red Dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
         />
-        <SectionCards title="Popular" videos={disneyVideos} size="small" />
+
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={USpopularVideos} size="small" />
+          <SectionCards title="인기" videos={KRpopularVideos} size="small" />
+        </div>
       </div>
     </div>
   );
