9877b953854292bd2ace5e9411feb27f3abd3e31
shinhonggyu <zowoz8819@gmail.com>
2022-05-22 16:40:14 +0900

    Sign-in Page Styling

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,18 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Login = () => {
+  const handleLoginWithEmail = (e) => {
+    e.preventDefault();
+    console.log("login");
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Netflix SignIn</title>
       </Head>
 
-      <header>
+      <header className={styles.header}>
         <div className={styles.headerWrapper}>
           <a className={styles.loginLink}>
             <div className={styles.logoWrapper}>
@@ -23,6 +28,23 @@ const Login = () => {
           </a>
         </div>
       </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+
+          <input
+            className={styles.emailInput}
+            type="text"
+            placeholder="Email address"
+          />
+
+          <p className={styles.userMsg}></p>
+          <button className={styles.loginBtn} onClick={handleLoginWithEmail}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
