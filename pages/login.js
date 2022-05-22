c9976a468cefb515b7a7d8c6bcebdfad42ab52c7
shinhonggyu <zowoz8819@gmail.com>
2022-05-22 14:03:48 +0900

    Sign-in Header Component

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,6 @@
+import styles from "../styles/Login.module.css";
 import Head from "next/head";
+import Image from "next/image";
 
 const Login = () => {
   return (
@@ -6,6 +8,21 @@ const Login = () => {
       <Head>
         <title>Netflix SignIn</title>
       </Head>
+
+      <header>
+        <div className={styles.headerWrapper}>
+          <a className={styles.loginLink}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="netflix logo"
+                width="128px"
+                height="34px"
+              />
+            </div>
+          </a>
+        </div>
+      </header>
     </div>
   );
 };
