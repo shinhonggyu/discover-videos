6d00971cff6e1bda7c31f36c7dbf468e22ed831e
shinhonggyu <zowoz8819@gmail.com>
2022-05-24 01:38:16 +0900

    Routing Flicker with Login

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { magic } from "../lib/magic-client";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleIsLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        router.push("/");
+      } else {
+        router.push("/login");
+      }
+    };
+
+    handleIsLoggedIn();
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChangeComplete = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />;
 }
 
 export default MyApp;
